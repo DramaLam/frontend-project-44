40e5f8dac71f9c9c75be1b6ef941567ca29f1925
Milena K <123456milena@rambler.ru>
2024-09-29 13:05:15 +0300

    UPDATE 1.2

--- src/games/runCalc.js
+++ src/games/runCalc.js
@@ -1,6 +1,24 @@
 import generationRandomNum from '../modules/generationRandomNum.js';
 import runGame from '../index.js';
 
+const performOperation = (num1, num2, operator) => {
+  let answer = '';
+
+  switch (operator) {
+    case '+':
+      answer = (num1 + num2).toString();
+      return answer;
+    case '-':
+      answer = (num1 - num2).toString();
+      return answer;
+    case '*':
+      answer = (num1 * num2).toString();
+      return answer;
+    default:
+      throw Error('Ой, что-то пошло не так. Повторите опрецию!');
+  }
+};
+
 const generateData = () => {
   const result = ['What is the result of the expression?'];
   const num1 = generationRandomNum(0, 100);
@@ -9,26 +27,12 @@ const generateData = () => {
   const operatorNum = generationRandomNum(0, 2);
   const operator = operatorsArr[operatorNum];
   const question = `${num1} ${operator} ${num2}`;
-  let answer = '';
+  const answer = performOperation(num1, num2, operator);
 
   result.push(question);
+  result.push(answer);
 
-  switch (operator) {
-    case '+':
-      answer = (num1 + num2).toString();
-      result.push(answer);
-      return result;
-    case '-':
-      answer = (num1 - num2).toString();
-      result.push(answer);
-      return result;
-    case '*':
-      answer = (num1 * num2).toString();
-      result.push(answer);
-      return result;
-    default:
-      throw Error('Ой, что-то пошло не так. Повторите опрецию!');
-  }
+  return result;
 };
 
 const runCalc = () => {
